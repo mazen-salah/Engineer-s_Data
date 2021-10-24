9b77b5a7ae7c1223ab7358a15121201261259249
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-10-24 23:21:02 +0200

    custom grid view item

--- lib/custom widgets/item_view.dart
+++ lib/custom widgets/item_view.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 
+class GridItem extends StatelessWidget {
+  final String id, title, image;
 
+  const GridItem(this.title, this.image, this.id, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(image), fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Text(title),
+          ),
+        ],
+      ),
+    );
+  }
+}
