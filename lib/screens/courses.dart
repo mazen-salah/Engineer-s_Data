899769258ebd9224448621766d3b9143b96be737
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-10-24 23:25:32 +0200

    courses screen

--- lib/screens/courses.dart
+++ lib/screens/courses.dart
@@ -3,3 +3,22 @@ import 'package:flutter/cupertino.dart';
 
 import '../app_data.dart';
 
+class CoursesPage extends StatelessWidget {
+  const CoursesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: coursesData
+          .map((courseData) =>
+          GridItem(courseData.title, courseData.image, courseData.id))
+          .toList(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+    );
+  }
+}
