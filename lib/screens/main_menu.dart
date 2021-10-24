a670d108cbf3bd2b71add92dfe337cc36a6c5aa8
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-10-24 23:23:45 +0200

    creating main menu screen

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -2,3 +2,14 @@ import 'package:engineers_data/custom%20widgets/item_view.dart';
 import 'package:flutter/material.dart';
 import '../app_data.dart';
 
+class MainMenu extends StatelessWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+        children:
+            mainMenuData.map((menuData) => ListItem(menuData.title)).toList());
+  }
+}
