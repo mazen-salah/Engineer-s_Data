65f42446f6a06a8fb31bdddb24591b0fb16c56cc
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-10-24 23:47:38 +0200

    Update home_view.dart

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -45,7 +45,7 @@ class _HomeState extends State<Home> {
           currentIndex: _currentIndex,
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(Icons.menu),
+              icon: Icon(Icons.home),
               label: "Main Menu",
             ),
             BottomNavigationBarItem(
