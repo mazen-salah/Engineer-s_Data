307cbe5a39a6cdffab9d52a09bd60215faccacb8
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-10-24 19:16:43 +0200

    add theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Engineer's Data",
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(brightness: Brightness.light)
+              .copyWith(secondary: Colors.blueGrey)),
+      darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(brightness: Brightness.dark)
+              .copyWith(secondary: Colors.blueGrey)),
     );
   }
 }
