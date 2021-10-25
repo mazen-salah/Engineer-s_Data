0623a5a1f004456824e03e34a9f98d54072266e8
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-10-25 02:22:07 +0200

    adding wallpaper, grid view

--- lib/screens/courses_page.dart
+++ lib/screens/courses_page.dart
@@ -1,5 +1,7 @@
 import 'package:engineers_data/custom%20widgets/item_view.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 import '../app_data.dart';
 
@@ -10,17 +12,43 @@ class CoursesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(25),
-      children: coursesData
-          .map((courseData) =>
-              GridItem(courseData.title, courseData.image, courseData.id))
-          .toList(),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-    );
+    return Stack(children: [
+      Image.asset(
+        'images/wp6587665.jpg',
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            const SafeArea(
+                child: Text(
+              'Courses:',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: "Pacifico"),
+            )),
+            Expanded(
+              child: GridView(
+                padding: const EdgeInsets.all(25),
+                children: coursesData
+                    .map((courseData) => GridItem(courseData.title,
+                        courseData.image, courseData.id, courseData.navId))
+                    .toList(),
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
   }
 }
