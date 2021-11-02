efee23b70290b6e774da1a433c4a01d67bf88e4b
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-11-02 04:32:09 +0200

    formatting

--- lib/app_data.dart
+++ lib/app_data.dart
@@ -14,40 +14,35 @@ const coursesData = [
       link:
           'https://drive.google.com/drive/folders/1V_1ZUzDJQZvYhnrIPdKcG2z40_5gIVy3',
       id: 'c2',
-      image:
-          'images/Mechanics.jpg',
+      image: 'images/Mechanics.jpg',
       navId: ''),
   Course(
       title: 'Circuits 1',
       link:
           'https://drive.google.com/drive/folders/1VRu-UWHQqJ8UdifNpe_6TkmOmyeUvHi7',
       id: 'C3',
-      image:
-          'images/circuits.png',
+      image: 'images/circuits.png',
       navId: ''),
   Course(
       title: 'Information transmission',
       link:
           'https://drive.google.com/drive/folders/1VU-_DZgBtEwS-7PmxPTmrvqORFkgtEB6',
       id: 'c4',
-      image:
-          'images/informationTransmission.jpg',
+      image: 'images/informationTransmission.jpg',
       navId: ''),
   Course(
       title: 'Energy',
       link:
           'https://drive.google.com/drive/folders/1VSMCle0DraMUjOQQGAWR0ofO1YQHyWyn',
       id: 'C5',
-      image:
-          'images/energy.jpg',
+      image: 'images/energy.jpg',
       navId: ''),
   Course(
       title: 'Structure',
       link:
           'https://drive.google.com/drive/folders/1VaeWKDM-bLXat9okGVIVZFC3u9kkSTqi',
       id: 'c6',
-      image:
-          'images/structures.jpg',
+      image: 'images/structures.jpg',
       navId: ''),
 ];
 
