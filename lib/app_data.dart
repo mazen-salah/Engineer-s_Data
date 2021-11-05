c6459b7a04f38750c50dcaed97f9427a21e3da30
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-11-05 05:35:10 +0200

    fixed #8

--- lib/app_data.dart
+++ lib/app_data.dart
@@ -4,46 +4,46 @@ import 'package:engineers_data/models/menu.dart';
 const coursesData = [
   Course(
       title: 'Math 3',
-      link:
+      url:
           'https://drive.google.com/drive/folders/1VV5Qdyh7rd0FYgEXRsPULR7_YE4PQwzl',
       id: 'C1',
       image: 'images/Math.jpeg',
-      navId: ''),
+      navId: 'Web'),
   Course(
       title: 'Mechanics 3',
-      link:
+      url:
           'https://drive.google.com/drive/folders/1V_1ZUzDJQZvYhnrIPdKcG2z40_5gIVy3',
       id: 'c2',
       image: 'images/Mechanics.jpg',
-      navId: ''),
+      navId: 'Web'),
   Course(
       title: 'Circuits 1',
-      link:
+      url:
           'https://drive.google.com/drive/folders/1VRu-UWHQqJ8UdifNpe_6TkmOmyeUvHi7',
       id: 'C3',
       image: 'images/circuits.png',
-      navId: ''),
+      navId: 'Web'),
   Course(
       title: 'Information transmission',
-      link:
+      url:
           'https://drive.google.com/drive/folders/1VU-_DZgBtEwS-7PmxPTmrvqORFkgtEB6',
       id: 'c4',
       image: 'images/informationTransmission.jpg',
-      navId: ''),
+      navId: 'Web'),
   Course(
       title: 'Energy',
-      link:
+      url:
           'https://drive.google.com/drive/folders/1VSMCle0DraMUjOQQGAWR0ofO1YQHyWyn',
       id: 'C5',
       image: 'images/energy.jpg',
-      navId: ''),
+      navId: 'Web'),
   Course(
       title: 'Structure',
-      link:
+      url:
           'https://drive.google.com/drive/folders/1VaeWKDM-bLXat9okGVIVZFC3u9kkSTqi',
       id: 'c6',
       image: 'images/structures.jpg',
-      navId: ''),
+      navId: 'Web'),
 ];
 
 const mainMenuData = [
