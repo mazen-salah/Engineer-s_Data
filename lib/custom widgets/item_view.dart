cb235df557a2489335560c9f083708d4d82b414c
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-11-07 06:48:00 +0200

    fix web view in web platform

--- lib/custom widgets/item_view.dart
+++ lib/custom widgets/item_view.dart
@@ -1,5 +1,7 @@
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class GridItem extends StatelessWidget {
   final String id, title, image, navId, url;
@@ -72,6 +74,13 @@ class ListItem extends StatelessWidget {
   }
 }
 
-void selectMenu(BuildContext context, navId, url) {
-  Navigator.of(context).pushNamed(navId, arguments: {'url': url});
+Future<void> selectMenu(BuildContext context, navId, url) async {
+  kIsWeb && navId == 'Web'
+      ? launch(
+          url,
+          forceSafariVC: true,
+          forceWebView: true,
+          enableJavaScript: true,
+        )
+      : Navigator.of(context).pushNamed(navId, arguments: {'url': url});
 }
