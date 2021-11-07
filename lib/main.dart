0cda1b505bed643f8ec77e870c305a4f6aea551f
mazen-salah <es-mazensalah2025@alexu.edu.eg>
2021-11-07 07:26:56 +0200

    updating web app name and icons

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Engineer's Data",
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: ThemeData(
